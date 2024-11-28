70c32b3d1c2ce4ea1f4bbdc252ca747efcab1cf1
LuisRojo18 <luisinrf18@gmail.com>
2024-11-28 09:32:56 -0500

    primer commit

---
+++ AlquilerCarros/ClienteUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerCarros
+{
+    public static class ClienteUtil
+
+    {
+
+        public static List<Cliente> Clientes = new List<Cliente>();
+
+        public static void Crear()
+
+        {
+
+            Console.Write("Ingrese el DNI del cliente: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int dni))
+
+            {
+
+                Console.WriteLine("DNI inválido. Intente nuevamente.");
+
+                return;
+
+            }
+
+
+
+            Console.Write("Ingrese el nombre del cliente: ");
+
+            string nombre = Console.ReadLine();
+        }
+    }
+}
+
+
+
+            
+
+  
