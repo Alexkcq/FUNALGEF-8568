f69e1e6a61185abdb4978909aab11bf760dfd6a8
ManuelSTP1201 <juanmasantacruz12@gmail.com>
2024-11-28 09:04:43 -0500

    Primer commit

---
+++ AlquilerCarros/Alquiler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AlquilerCarros
+{
+    public class Alquiler
+
+    {
+
+        // Propiedades
+
+        public int Numero { get; set; }
+
+        public Carro CarroAlquilado { get; set; }
+
+        public Cliente Contratante { get; set; }
+
+        public double Precio { get; set; }
+
+
+
+        // Constructor opcional
+
+        public Alquiler(int numero, Carro carroAlquilado, Cliente contratante, double precio)
+
+        {
+
+            Numero = numero;
+
+            CarroAlquilado = carroAlquilado;
+
+            Contratante = contratante;
+
+            Precio = precio;
+
+        }
+
+        public override string ToString()
+
+        {
+
+            return $"Número: {Numero}, Carro: [{CarroAlquilado}], Contratante: [{Contratante}], Precio: {Precio:C}";
+
+        }
+
+    }
+}
