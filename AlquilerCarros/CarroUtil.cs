28dff724fa44dff75887b22e00ee35489f16a086
katty0306 <kattymm635@gmail.com>
2024-11-28 09:40:12 -0500

    Tercer commit

--- AlquilerCarros/CarroUtil.cs
+++ AlquilerCarros/CarroUtil.cs
@@ -6,7 +6,89 @@ using System.Threading.Tasks;
 
 namespace AlquilerCarros
 {
-    internal class CarroUtil
+
+    using System;
+
+    using System.Collections.Generic;
+
+
+
+    public static class CarroUtil
+
     {
-    }
-}
+
+
+
+        public static List<Carro> Carros = new List<Carro>();
+
+
+
+        public static void Crear()
+
+        {
+
+            Console.Write("Ingrese la placa del carro: ");
+
+            string placa = Console.ReadLine();
+
+
+
+            Console.Write("Ingrese la marca del carro: ");
+
+            string marca = Console.ReadLine();
+
+
+
+            Console.Write("Ingrese el precio de alquiler del carro: ");
+
+            if (!double.TryParse(Console.ReadLine(), out double precioAlquiler))
+
+            {
+
+                Console.WriteLine("Precio inválido. Intente nuevamente.");
+
+                return;
+
+            }
+
+
+
+            Carro nuevoCarro = new Carro(placa, marca, precioAlquiler);
+
+            Carros.Add(nuevoCarro);
+
+
+
+            Console.WriteLine("¡Carro agregado exitosamente!");
+
+        }
+
+
+
+        public static void Listar()
+
+        {
+
+            if (Carros.Count == 0)
+
+            {
+
+                Console.WriteLine("No hay carros registrados.");
+
+                return;
+
+            }
+
+
+
+            Console.WriteLine("Listado de carros:");
+
+            foreach (var carro in Carros)
+
+            {
+
+                Console.WriteLine(carro);
+
+            }
+
+        }
